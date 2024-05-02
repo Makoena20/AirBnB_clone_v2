3320ecac5da8ee7a5ae1e9ab0bed99f1746c2937
Makoena20 <koenaletseku@gmail.com>
2024-05-02 01:55:20 -0700

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
+-- Script to prepare MySQL server for the project
+
+-- Create database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create user if it doesn't exist and set password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user on the database
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
 

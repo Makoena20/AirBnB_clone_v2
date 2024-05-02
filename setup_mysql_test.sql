366623f9e6b2e21359fa4c947d1d05ac855245a8
Makoena20 <koenaletseku@gmail.com>
2024-05-02 02:04:25 -0700

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-This script prepares a MySQL server for the project:
 
-- Creates a database hbnb_test_db
-- Creates a new user hbnb_test (in localhost)
-- Sets the password of hbnb_test to hbnb_test_pwd
-- Grants all privileges on hbnb_test_db to hbnb_test
-- Grants SELECT privilege on performance_schema to hbnb_test
+-- Script to prepare MySQL server for the project
 
-If the database hbnb_test_db or the user hbnb_test already exists, the script will not fail.
-"""
-
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant privileges to the user for the database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 

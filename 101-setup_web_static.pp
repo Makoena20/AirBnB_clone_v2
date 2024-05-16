cd485adaf36755007fd33da97d120b4e2a3fd4bb
Makoena20 <koenaletseku@gmail.com>
2024-05-16 05:13:27 -0700

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,36 +1,46 @@
-# Puppet Manifest to set up web_static directory
+# 101-setup_web_static.pp
+
+# Ensure Nginx is installed
+package { 'nginx':
+  ensure => installed,
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Define web_static directory
 file { '/data':
   ensure => 'directory',
 }
 
-file { '/data/web_static':
+# Define web_static/releases and web_static/shared directories
+file { ['/data/web_static/releases', '/data/web_static/shared']:
   ensure => 'directory',
 }
 
-file { '/data/web_static/releases':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory',
-}
-
-file { '/data/web_static/releases/test/index.html':
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>',
-}
-
+# Create a symbolic link from current to releases/test
 file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test',
+  require => File['/data/web_static/releases'],
 }
+
+# Create index.html file inside releases/test directory
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'file',
+  content => '<html>\n<head>\n</head>\n<body>\n  Holberton School\n</body>\n</html>\n',
+  require => File['/data/web_static/releases/test'],
+}
+
+# Restart Nginx if any file in the /data directory changes
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/data'],
+}
+
 

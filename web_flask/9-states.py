2ab544b88e384553cd290b84a09202bcdb569c15
Makoena20 <koenaletseku@gmail.com>
2024-05-31 08:34:34 +0000

    9-states.py

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Teardown method to close the database
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def list_states():
+    """
+    Route that displays a HTML page with a list of all State objects
+    """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('9-states.html', states=sorted_states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def display_state(id):
+    """
+    Route that displays a HTML page with information about a State
+    """
+    state = storage.get(State, id)
+    if state:
+        cities = sorted(state.cities, key=lambda city: city.name)
+        return render_template('9-states.html', state=state, cities=cities)
+    else:
+        return render_template('9-states.html', not_found=True)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
